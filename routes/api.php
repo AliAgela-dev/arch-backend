4fe260e77ac24e9e6a8c947e3ea2778affb50423
Zahra-E <zahraelgaoud@gmail.com>
2025-12-27 15:06:13 +0200

    Completed routes with scramble

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\FacultyController;
+use App\Http\Controllers\Admin\ProgramController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Faculty Routes
+Route::prefix('faculties')->group(function () {
+    Route::get('/', [FacultyController::class, 'index']);
+    Route::get('/{id}', [FacultyController::class, 'show']);
+    Route::post('/', [FacultyController::class, 'store']);
+    Route::put('/{id}', [FacultyController::class, 'update']);
+    Route::delete('/{id}', [FacultyController::class, 'destroy']);
+    Route::post('/{id}/restore', [FacultyController::class, 'restore']);
+});
+
+// Program Routes
+Route::prefix('programs')->group(function () {
+    Route::get('/', [ProgramController::class, 'index']);
+    Route::get('/{id}', [ProgramController::class, 'show']);
+    Route::post('/', [ProgramController::class, 'store']);
+    Route::put('/{id}', [ProgramController::class, 'update']);
+    Route::delete('/{id}', [ProgramController::class, 'destroy']);
+    Route::post('/{id}/restore', [ProgramController::class, 'restore']);
+});
